7d8b2014ba730380380b956f97f160147b7589c0
arttuhal <38346288+arttuhal@users.noreply.github.com>
2018-04-20 11:44:10 +0300

    Add files via upload

---
+++ Scripts/GoldPileScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPileScript : MonoBehaviour {
+
+	public GameObject goldPile;
+	float x = Mathf.PI / 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, (float)(transform.position.y + (Mathf.Sin (x)) / 100), transform.position.z);
+		x += Mathf.PI / 100;
+		if (x >= Mathf.PI) {
+			x += Mathf.PI / 100;
+		}
+	}
+}
