38db09d54f8fde87c5272010dfde71d734f50e77
arttuhal <38346288+arttuhal@users.noreply.github.com>
2018-04-20 11:38:17 +0300

    Add files via upload

--- Scripts/ChangeScene.cs
+++ Scripts/ChangeScene.cs
@@ -7,6 +7,12 @@ public class ChangeScene : MonoBehaviour {
 	
 	public static void StringManager(string level) {
 		SceneManager.LoadScene (level);
+		Score.startScore = Score.score;
 
 	}
+
+	public static void Restart (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
 }
